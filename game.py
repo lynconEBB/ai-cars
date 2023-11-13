3be1e591c02dc39957e7c85636c04beeb7c04120
Lyncon Baez <lyncon.baez@pti.org.br>
2023-11-13 00:21:17 -0300

    Drawing radars

--- game.py
+++ game.py
@@ -9,10 +9,11 @@ HEIGHT = 1080
 
 BORDER_COLOR = (255, 255, 255, 255)  # Color To Crash on Hit
 
+
 def blit_rotate_center(screen, image, top_left, angle):
     rotated_image = pygame.transform.rotate(image, angle)
-    new_rect = rotated_image.get_rect(center=image.get_rect(topleft=top_left).center)
-    screen.blit(rotated_image, new_rect.topleft)
+    new_rect = rotated_image.get_rect(center=top_left)
+    screen.blit(rotated_image, new_rect)
 
 
 class Car:
@@ -21,16 +22,17 @@ class Car:
         self.sprite = pygame.image.load('./imgs/green.png')
         self.sprite = pygame.transform.scale_by(self.sprite, 2)
         self.acceleration = 0.1
-        self.break_strenght = 0.8
+        self.break_strength = 0.8
         self.max_speed = 10
 
-        self.position = [830, 900]
+        self.position = [830, 950]
         self.angle = -90
         self.speed = 0
-        self.angular_speed = 5
+        self.angular_speed = 2.5
 
-        self.radars = []
-        self.drawing_radars = []
+        self.radar_count = 8
+        self.radar_dists = []
+        self.radar_end_positions = []
 
         self.alive = True
 
@@ -42,31 +44,37 @@ class Car:
         self.draw_radar(screen)
 
     def draw_radar(self, screen):
-        for radar in self.radars:
-            position = radar[0]
-            pygame.draw.line(screen, (0, 255, 0), self.center, position, 1)
-            pygame.draw.circle(screen, (0, 255, 0), position, 5)
+        for radar in self.radar_end_positions:
+            radar_end = radar
+            pygame.draw.line(screen, (0, 255, 0), self.position, radar_end, 1)
+            pygame.draw.circle(screen, (0, 255, 0), radar_end, 5)
 
     def is_colliding(self, track_mask):
-        car_mask = pygame.mask.from_surface(self.sprite)
-        offset = (self.position[0], self.position[1])
-        return track_mask.overlap(car_mask, offset)
+        rotated_sprite = pygame.transform.rotate(self.sprite, self.angle)
+        draw_rect = rotated_sprite.get_rect(center=self.position)
+        car_mask = pygame.mask.from_surface(rotated_sprite)
+        return track_mask.overlap(car_mask, draw_rect.topleft)
 
+    def check_radars(self, map_mask):
+        radar_angle_offset = self.angle
+        self.radar_end_positions.clear()
+        self.radar_dists.clear()
 
-    def check_radar(self, degree, game_map):
-        length = 0
-        x = int(self.center[0] + math.cos(math.radians(360 - (self.angle + degree))) * length)
-        y = int(self.center[1] + math.sin(math.radians(360 - (self.angle + degree))) * length)
+        for i in range(0, self.radar_count):
+            length = 0
+            x = int(self.position[0] + math.cos(math.radians(360 - radar_angle_offset)))
+            y = int(self.position[1] + math.sin(math.radians(360 - radar_angle_offset)))
 
-        # While We Don't Hit BORDER_COLOR AND length < 300 (just a max) -> go further and further
-        while not game_map.get_at((x, y)) == BORDER_COLOR and length < 300:
-            length = length + 1
-            x = int(self.center[0] + math.cos(math.radians(360 - (self.angle + degree))) * length)
-            y = int(self.center[1] + math.sin(math.radians(360 - (self.angle + degree))) * length)
+            while not map_mask.get_at((x, y)) == 1 and length < 300:
+                length = length + 1
+                x = int(self.position[0] + math.cos(math.radians(360 - radar_angle_offset)) * length)
+                y = int(self.position[1] + math.sin(math.radians(360 - radar_angle_offset)) * length)
 
-        # Calculate Distance To Border And Append To Radars List
-        dist = int(math.sqrt(math.pow(x - self.center[0], 2) + math.pow(y - self.center[1], 2)))
-        self.radars.append([(x, y), dist])
+            dist = int(math.sqrt(math.pow(x - self.position[0], 2) + math.pow(y - self.position[1], 2)))
+            self.radar_end_positions.append((x, y))
+            self.radar_dists.append(dist)
+
+            radar_angle_offset += 180 / (self.radar_count - 1)
 
     def rotate(self, left=False, right=False):
         if left:
@@ -80,24 +88,15 @@ class Car:
     def reduce_speed(self):
         self.speed = max(self.speed - self.acceleration, 0)
 
+    def active_break(self):
+        self.speed = max(self.speed - self.break_strength, 0)
+
     def update(self, game_map):
         self.distance += self.speed
         self.time += 1
 
         self.position[1] -= math.cos(math.radians(self.angle)) * self.speed
         self.position[0] -= math.sin(math.radians(self.angle)) * self.speed
-
-        # for d in range(-90, 120, 45):
-        # self.check_radar(d, game_map)
-
-    def get_data(self):
-        # Get Distances To Border
-        radars = self.radars
-        return_values = [0, 0, 0, 0, 0]
-        for i, radar in enumerate(radars):
-            return_values[i] = int(radar[1] / 30)
-
-        return return_values
 
     def is_alive(self):
         return self.alive
@@ -113,6 +112,8 @@ def poll_events(car):
     if keys[pygame.K_UP]:
         car.add_speed()
         moved = True
+    if keys[pygame.K_DOWN]:
+        car.active_break()
     if keys[pygame.K_LEFT]:
         car.rotate(left=True)
     if keys[pygame.K_RIGHT]:
@@ -125,11 +126,13 @@ def poll_events(car):
 # Main Code
 pygame.init()
 screen = pygame.display.set_mode((WIDTH, HEIGHT), pygame.FULLSCREEN)
-
 clock = pygame.time.Clock()
 
 game_map = pygame.image.load('./imgs/map.png').convert()
-MAP_MASK = pygame.mask.from_surface(game_map)
+MAP_MASK = game_map.copy()
+MAP_MASK.set_colorkey((255, 255, 255))
+MAP_MASK = pygame.mask.from_surface(MAP_MASK)
+MAP_MASK.invert()
 
 car = Car()
 
@@ -143,6 +146,7 @@ while True:
 
     poll_events(car)
     car.update(game_map)
+    car.check_radars(MAP_MASK)
 
     if car.is_colliding(MAP_MASK):
         car = Car()
